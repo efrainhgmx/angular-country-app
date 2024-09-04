2bbd5b33e2e3afa001fdae42927a71f67013a4f4
Efrain Hernandez <efrainhg2050@gmail.com>
2024-09-04 14:01:31 -0600

    Adding Contact Page component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,23 +4,16 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { RouterModule } from '@angular/router';
-
-
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 
 @NgModule({
   declarations: [
     AboutPageComponent,
+    ContactPageComponent,
     HomePageComponent,
-    SidebarComponent
+    SidebarComponent,
   ],
-  imports: [
-    CommonModule,
-    RouterModule
-  ],
-  exports: [
-    AboutPageComponent,
-    HomePageComponent,
-    SidebarComponent
-  ]
+  imports: [CommonModule, RouterModule],
+  exports: [AboutPageComponent, HomePageComponent, SidebarComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
