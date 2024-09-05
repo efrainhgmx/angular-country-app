ad47fe956e2b96771c3ef16436f4311177fd7ff5
Efrain Hernandez <efrainhg2050@gmail.com>
2024-09-05 13:04:09 -0600

    Using outputs in search box component

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,11 +1,30 @@
-import { Component, Input } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  Output,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'shared-search-box',
-  templateUrl: './search-box.component.html'
+  templateUrl: './search-box.component.html',
 })
 export class SearchBoxComponent {
-
   @Input()
-  public placeholder:string = 'Realizar busqueda...';
+  public placeholder: string = 'Realizar busqueda...';
+
+  @Output()
+  public onValue: EventEmitter<string> = new EventEmitter();
+
+  @ViewChild('txtSearchInput')
+  private searchInputElement!: ElementRef<HTMLInputElement>;
+
+  searchValue(): void {
+    const searchTerm: string = this.searchInputElement.nativeElement.value;
+    if (!searchTerm) return;
+
+    this.onValue.emit(searchTerm);
+  }
 }
