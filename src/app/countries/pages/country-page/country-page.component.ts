98ebeef0a1007e62953eba3f1d9f690125616c5e
Efrain Hernandez <efrainhg2050@gmail.com>
2024-09-06 18:41:22 -0600

    Using switchMap and map operator to evit obsevers hell

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { CountriesService } from '../../services/countries.service';
+import { map, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-country-page',
@@ -10,14 +11,22 @@ export class CountryPageComponent implements OnInit{
 
   constructor( 
     private acivatedRoute: ActivatedRoute,
-    private countriesService: CountriesService
+    private countriesService: CountriesService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
     this.acivatedRoute.params
-    .subscribe(({ id }) => {
-      this.countriesService.searchCountryByAlphaCode(id)
-      .subscribe(console.log)
+    .pipe(
+      switchMap( ({ id }) =>  this.countriesService.searchCountryByAlphaCode(id)),
+      map( countries => countries.length > 0 ? countries[0] : null)
+    )
+    .subscribe( country => {
+      if(!country) {
+        return this.router.navigateByUrl('');
+      } else {
+        return;
+      }
     })
   }
 }
