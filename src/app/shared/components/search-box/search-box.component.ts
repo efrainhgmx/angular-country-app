d3b16ac5c7dcf5c4e8da589d01217d2242ee59d5
Efrain Hernandez <efrainhg2050@gmail.com>
2024-09-30 18:26:42 -0600

    Cleaning debapuncer subscription

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,31 +1,35 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { debounceTime, Subject } from 'rxjs';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { debounceTime, Subject, Subscription } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
   templateUrl: './search-box.component.html',
 })
-export class SearchBoxComponent implements OnInit {
-
+export class SearchBoxComponent implements OnInit, OnDestroy {
+  
   private debouncer: Subject<string> = new Subject<string>();
-
+  private debouncerSubscription?: Subscription;
+  
   @Input()
   public placeholder: string = 'Realizar busqueda...';
-
+  
   @Output()
   public onValue = new EventEmitter<string>();
-
+  
   @Output()
   public onDebounce = new EventEmitter<string>();
-
+  
   ngOnInit(): void {
-    this.debouncer
+    this.debouncerSubscription = this.debouncer
     .pipe(
       debounceTime(1200)
     )
-    .subscribe( value => this.onDebounce.emit(value))
+    .subscribe( value => this.onDebounce.emit(value));
   }
-
+  
+  ngOnDestroy(): void {
+    this.debouncerSubscription?.unsubscribe();
+  }
 
   emitValue( value: string ):void  {
     this.onValue.emit(value);
