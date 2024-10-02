b2231a61551773b5c4894fee95624efef2b14454
Efrain Hernandez <efrainhg2050@gmail.com>
2024-10-02 16:43:00 -0600

    Adding types and button in region page

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Country } from '../../interfaces/country.interface';
 import { CountriesService } from '../../services/countries.service';
 
+type Region = 'Americas'| 'Asia'| 'Africa'| 'Europe'| 'Oceania';
 @Component({
   selector: 'app-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -9,6 +10,7 @@ import { CountriesService } from '../../services/countries.service';
 export class ByRegionPageComponent {
   public countries: Country[] = [];
   public isLoading: boolean = false;
+  public regions: Region[] = ['Americas', 'Asia', 'Africa', 'Europe', 'Oceania'];
 
   constructor(private countriesService: CountriesService) {}
 
