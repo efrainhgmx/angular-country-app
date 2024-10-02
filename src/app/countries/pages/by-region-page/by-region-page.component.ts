cfc8f13532824164d2fa525f11ee1737a4856cd8
Efrain Hernandez <efrainhg2050@gmail.com>
2024-10-02 16:46:22 -0600

    Adding ng class for better user experience

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -11,13 +11,14 @@ export class ByRegionPageComponent {
   public countries: Country[] = [];
   public isLoading: boolean = false;
   public regions: Region[] = ['Americas', 'Asia', 'Africa', 'Europe', 'Oceania'];
+  public selectRegion?: Region;
 
   constructor(private countriesService: CountriesService) {}
 
-  searchByRegion(region: string): void {
-    region = region.trim();
+  searchByRegion(region: Region): void {
     if(!region) return;
     this.isLoading = true;
+    this.selectRegion = region;
     this.countriesService
       .searchRegionCountries(region)
       .subscribe((countriesResponse) => {
